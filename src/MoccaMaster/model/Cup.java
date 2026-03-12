520a6bc3f124327a9a627ec5138b969e020a9ff8
celineewu <celineewu@gmail.com>
2026-03-12 10:37:53 +0100

    update

--- src/MoccaMaster/model/Cup.java
+++ src/MoccaMaster/model/Cup.java
@@ -8,6 +8,31 @@ Den indeholder en Drink.
 */
 
 public class Cup {
+    private Drink drink;
+
+    public Cup(Drink drink) {
+        this.drink = drink;
+    }
+
+    public Drink getDrink() {
+        return drink;
+    }
+
+    public String describe() {
+        return " | Drink: " + getDrink() +
+                " | Size: " + drink.getSize();
+    }
+}
+
+        /*boolean getWhippedCream = true;
+        if (getWhippedCream){
+            System.out.println("With Whipped Cream.");
+
+    }
+
+    }
+
+
 
     /*
     STUDENT TASK:
@@ -15,12 +40,12 @@ public class Cup {
     Opret en variabel:
     Drink drink
     */
-
     /*
     STUDENT TASK:
 
     Skriv en constructor der modtager en Drink.
     */
+
 
     /*
     STUDENT TASK:
@@ -39,14 +64,12 @@ public class Cup {
 
     /*
     HER SKAL VÆRE EN IF
-
     Hvis størrelse ikke er null.
     */
 
     /*
     HER SKAL VÆRE EN IF
-
     Hvis der er flødeskum.
     */
 
-}
+
