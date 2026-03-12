3a37f71fe2d750d7e6539e766914083489b378df
ispa070 <59nyd8m94g@privaterelay.appleid.com>
2026-03-12 10:26:21 +0100

    CoffeeDrink third draft

--- src/MoccaMaster/model/CoffeeDrink.java
+++ src/MoccaMaster/model/CoffeeDrink.java
@@ -8,9 +8,11 @@ Dette er et eksempel på arv (inheritance).
 */
 
 public class CoffeeDrink extends Drink {
+    super.whippedCream = false;
 
-    public CoffeeDrink(DrinkType type, Size size) {
+    public CoffeeDrink(DrinkType type, Size size, boolean whippedCream) {
         super(type, size);
+        this.whippedCream = whippedCream;
     }
 
     @Override
